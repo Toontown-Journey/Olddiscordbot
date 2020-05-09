0d84c273b1a4cf052490a1d512a2732431a1df8e
Anonymous13 <mgracer48@yahoo.com>
2020-05-09 23:17:22 +0200

    Main: Progress on toon+ role

--- main.py
+++ main.py
@@ -3,9 +3,12 @@ from discord.ext import commands
 import asyncio
 import os
 import config
+from datetime import date
+import MySQLdb
 
 bot = commands.Bot(command_prefix=commands.when_mentioned_or('!'), description='discord bot')
-
+db = MySQLdb.connect("198.54.125.59","anonuwzz_discord","test123","anonuwzz_discord" )
+cursor = db.cursor
 @bot.event
 async def on_ready():
     print('Logging in...')
@@ -13,11 +16,39 @@ async def on_ready():
     print('id: ' + str(bot.user.id))
     load_cogs()
 
+def calculateTime(join_time):
+    date1 = date.today()
+    date2 = join_time
+    weeks = ( date1- date2).days// 7
+    return weeks
+
+def getTotalMessages(member):
+    return 
+
+def setTotalMessages(member):
+    return 
+    data = {
+        member: member.totalMessages
+    }
+    with io.open('data.yaml', w, ecoding='utf8') as f:
+        yaml.dump(data, f, default_flow_style=False, allow_unicode=True)
+        
 @bot.event
 async def on_message(message):
     if message.channel.id == 708507541784494111:
         await message.add_reaction('✅')
         await message.add_reaction('❌')
+
+    return
+    member = message.author
+    memeber.totalMessages = getTotalMessages(member)
+    member.totalMessages += 1
+
+    setTotalMessages(member)
+    weeks = calculateTime(member.joined_at)
+    if weeks >= 2 and member.totalMessages >= 100:
+       await member.add_roles(discord.utils.get(member.guild.roles, name='Toons+'))
+
 
 def update_avatar(filename):
     if os.path.isfile(filename):
