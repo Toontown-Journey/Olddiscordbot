4f05798e41fd839788bc8e8a42828270186c7de4
Anonymous13 <mgracer48@yahoo.com>
2020-05-11 02:18:35 +0200

    Rolemanager: Fix getting experience, setting new entry for experience, and calculating week

--- cogs/Rolemanager.py
+++ cogs/Rolemanager.py
@@ -1,7 +1,6 @@
 #  Copyright (c) 2020. Toontown Journey. All rights reserved
 
-from datetime import date
-
+import datetime 
 import MySQLdb
 import discord
 from discord.ext import commands
@@ -16,7 +15,7 @@ from random import randrange
 
 
 def calculate_time(join_time):
-    date1 = date.today()
+    date1 = datetime.datetime.now()
     date2 = join_time
     weeks = (date1 - date2).days // 7
     return weeks
@@ -37,41 +36,44 @@ class Rolemanager(commands.Cog):
                                  remote_bind_address=('localhost', config.sshPort)) as tunnel:
             """
         self.db = MySQLdb.connect(config.mysqlHost, config.mysqlUsername, config.mysqlPassword, config.mysqlDatabase)
-        self.cursor = self.db.cursor
+        self.cursor = self.db.cursor()
 
 
 
     def get_total_experience(self, member):
         sql = 'SELECT * FROM experience'
-        try:
-            self.cursor.execute(sql)
+        #try:
+        self.cursor.execute(sql)
 
-            results = self.cursor.fetchall()
-            for row in results:
-                resultsMember = row[0]
-                totalExperience = row[1]
-                if member == resultsMember:
-                    return totalExperience
-                
-            return "New user"
-        except:
-            print('Error: Unable to fetch data')
+        results = self.cursor.fetchall()
+        for row in results:
+            resultsMember = row[0]
+            totalExperience = row[1]
+            if member == resultsMember:
+                return totalExperience
+            
+        return "New user"
+        #except:
+           # print('Error: Unable to fetch data')
+          #  return "New user"
 
     def set_total_experience(self, member, totalExperience):
-        sql = "INSERT INTO experience( " + member  + ','  + " " + totalExperience + ")"
-        try:
-            self.cursor.execute(sql)
-            self.db.commit()
-        except:
-            self.db.rollback()
+        sql = ("INSERT INTO experience "
+             "(member, xp) "
+             "VALUES ({0}, {1})").format(member, totalExperience)
+       # try:
+        self.cursor.execute(sql)
+        self.db.commit()
+       # except:
+        #    self.db.rollback()
 
     def update_total_experience(self, member, totalExperience):
-        sql = "UPDATE experience SET xp = " + totalExperience + " WHERE member = " + member
-        try:
-            self.cursor.execute(sql)
-            self.db.commit()
-        except:
-            self.db.rollback()
+        sql = "UPDATE experience SET xp = " + str(totalExperience) + " WHERE member = " + str(member)
+        #try:
+        self.cursor.execute(sql)
+        self.db.commit()
+        #except:
+            #self.db.rollback()
 
     @commands.Cog.listener()
     async def on_message(self, message):
@@ -79,19 +81,20 @@ class Rolemanager(commands.Cog):
         if message.channel.id == 708507541784494111:
             await message.add_reaction('✅')
             await message.add_reaction('❌')
-        member = message.author.id
-        if self.get_total_experience(member) == 'New user':
+        member = message.author
+        memberId = message.author.id
+        if self.get_total_experience(memberId) == 'New user':
            #No entry for this member, create a new one
-            member.totalExperience = 0
-            self.set_total_experience(member, member.totalExperience)
+            memberTotalExperience = 0
+            self.set_total_experience(memberId, memberTotalExperience)
         else:
-            member.totalExperience = self.get_total_experience(member)
+            memberTotalExperience = self.get_total_experience(memberId)
  
-        member.totalExperience += randrange(1,10)
+        memberTotalExperience += randrange(1,10)
 
-        self.update_total_experience(member, member.totalExperience)
+        self.update_total_experience(memberId, memberTotalExperience)
         weeks = calculate_time(member.joined_at)
-        if weeks >= 2 and member.totalExperience >= 500:
+        if weeks >= 2 and memberTotalExperience >= 500:
             await member.add_roles(discord.utils.get(member.guild.roles, name='Toons+'))
 
 
